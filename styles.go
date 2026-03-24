07c5054707339353986a4f3ac144da6ea8928142
chuma-beep <wisboynelson123@gmail.com>
2026-03-24 18:31:38 +0100

    updated sytling

--- styles.go
+++ styles.go
@@ -4,25 +4,36 @@ import "github.com/charmbracelet/lipgloss"
 
 // Catppuccin Mocha palette
 var (
-	correctStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("#a6e3a1"))
-	incorrectStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#f38ba8"))
-	pendingStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("#45475a"))
-	cursorStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("#cdd6f4")).Underline(true)
+	correctStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("#a6e3a1"))
+	incorrectStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#f38ba8"))
+	pendingStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("#45475a"))
+	cursorStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("#cdd6f4")).Underline(true)
 
-	titleStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("#cba6f7")).Bold(true).MarginBottom(1)
-	wpmStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("#f9e2af")).Bold(true)
-	accStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("#89dceb")).Bold(true)
-	timeStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#89b4fa"))
-	subtleStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("#585b70"))
-	hintStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#585b70"))
-	pbStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("#1e1e2e")).Background(lipgloss.Color("#f9e2af")).Bold(true)
+	titleStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#cba6f7")).Bold(true).MarginBottom(1)
+	wpmStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("#f9e2af")).Bold(true)
+	accStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("#89dceb")).Bold(true)
+	timeStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("#89b4fa"))
+	subtleStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("#585b70"))
+	hintStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("#313244"))
+	pbStyle          = lipgloss.NewStyle().Foreground(lipgloss.Color("#1e1e2e")).Background(lipgloss.Color("#f9e2af")).Bold(true)
+	errorStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#f38ba8"))
 
-	// Menu styles
-	selectedStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#1e1e2e")).Background(lipgloss.Color("#cba6f7")).Bold(true).Padding(0, 1).MarginRight(1)
+	// Menu
+	selectedStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("#1e1e2e")).Background(lipgloss.Color("#cba6f7")).Bold(true).Padding(0, 1).MarginRight(1)
 	dimSelectedStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#1e1e2e")).Background(lipgloss.Color("#585b70")).Bold(true).Padding(0, 1).MarginRight(1)
-	optionStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("#585b70")).Padding(0, 1).MarginRight(1)
+	optionStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("#585b70")).Padding(0, 1).MarginRight(1)
 
-	errorStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("#f38ba8"))
+	// Syntax highlight (pending chars in code mode)
+	hlKeyword  = lipgloss.NewStyle().Foreground(lipgloss.Color("#cba6f7"))
+	hlBuiltin  = lipgloss.NewStyle().Foreground(lipgloss.Color("#89dceb"))
+	hlString   = lipgloss.NewStyle().Foreground(lipgloss.Color("#a6e3a1"))
+	hlComment  = lipgloss.NewStyle().Foreground(lipgloss.Color("#6c7086"))
+	hlNumber   = lipgloss.NewStyle().Foreground(lipgloss.Color("#fab387"))
+	hlPunct    = lipgloss.NewStyle().Foreground(lipgloss.Color("#89b4fa"))
+
+	// WPM sparkline
+	sparkBarStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#cba6f7"))
+	sparkPeakStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#f9e2af"))
 
 	cardStyle = lipgloss.NewStyle().
 		Border(lipgloss.RoundedBorder()).
