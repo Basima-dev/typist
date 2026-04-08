2de9d7ea65e4c96e1cf934f72e3baec106b1fdbb
chuma-beep <wisboynelson123@gmail.com>
2026-04-08 11:43:50 +0100

    added igonre offset in the second line

--- words_test.go
+++ words_test.go
@@ -18,19 +18,19 @@ func TestGenerateWords(t *testing.T) {
 	}
 }
 
-// func TestWrapIntoLines(t *testing.T) {
-// 	text := "This is a simple test sentence for the typing app to check if line wrapping works correctly when the text is long enough."
-//
-// 	lines := wrapIntoLines(text, 40)
-//
-// 	if len(lines) == 0 {
-// 		t.Fatal("wrapIntoLines returned no lines")
-// 	}
-//
-// 	for i, line := range lines {
-// 		if len(line) > 40 {
-// 			t.Errorf("Line %d is too long (%d characters): %s", i, len(line), line)
-// 		}
-// 	}
-// }
-//
+ func TestWrapIntoLines(t *testing.T) {
+ 	text := "This is a simple test sentence for the typing app to check if line wrapping works correctly when the text is long enough."
+
+ 	lines,_ := wrapIntoLines(text, 40)
+
+ 	if len(lines) == 0 {
+ 		t.Fatal("wrapIntoLines returned no lines")
+ 	}
+
+ 	for i, line := range lines {
+ 		if len(line) > 40 {
+ 			t.Errorf("Line %d is too long (%d characters): %s", i, len(line), line)
+ 		}
+	}
+ }
+
